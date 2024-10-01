33da2d6038b589910c978f4628221020623e31c8
Humayun <humayunahmad987@gmail.com>
2024-10-02 03:18:05 +0530

    rendering frontend from server itself

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from "path";
 import cookieParser from 'cookie-parser';
 import connectDB from './db/connectDb.js'
 // to use the routes we created in routes folder using middlewares
@@ -9,16 +10,25 @@ import dotenv from 'dotenv';
 import {app, server} from "./socket/socket.js";
 // we cannot directly use environment variables into our code unless we do dotenv.config() 
 dotenv.config()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 app.use(express.json()); // to parse incoming request from JSON payloads... from req.body
 app.use(cookieParser());
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
 
-app.get('/',(req,res)=>{
-    res.send("Hello World how are you")
+// since we are listening to this middleware we can render or connect to our frontend from the server itself
+// by using express static and path 
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*", (req,res)=>{
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
 })
+
+// app.get('/',(req,res)=>{
+//     res.send("Hello World how are you")
+// })
 
 server.listen(PORT,()=>{
     connectDB()
