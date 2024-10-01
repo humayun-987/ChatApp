104dcd76b45faeb454b198a38ba8a1f7131e4199
Humayun <humayunahmad987@gmail.com>
2024-10-02 03:43:50 +0530

    Socket url updated

--- frontend/src/context/SocketCOntext.jsx
+++ frontend/src/context/SocketCOntext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
   const { authUser } = useAuthContext()
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://chatapp-prod-n4cd.onrender.com", {
         query: {
           userId: authUser._id
         }
